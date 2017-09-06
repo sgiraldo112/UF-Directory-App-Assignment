a5eafc55b3e25d2c2c17b189c36f015e9c182b11
sgiraldo112 <31422438+sgiraldo112@users.noreply.github.com>
2017-09-06 19:07:51 -0400

    Add files via upload

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -7,15 +7,27 @@ var fs = require('fs'),
     mongoose = require('mongoose'), 
     Schema = mongoose.Schema, 
     Listing = require('./ListingSchema.js'), 
-    config = require('./config');
+    config = require('./config'),
+	showTheLists = require('./listings')
 
 /* Connect to your database */
-
+mongoose.connect(config.db.uri);
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
  */
-
+for(var i = 0; i < showTheLists.entries.length; i++){
+	var newListing = Listing({
+		code: showTheLists.entries[i].code,
+		name: showTheLists.entries[i].name,
+		coordinates: showTheLists.entries[i].coordinates,
+		address: showTheLists.entries[i].address
+	});
+	
+	newListing.save(function(err){
+		if(err) throw err;
+	});
+}
 
 /* 
   Once you've written + run the script, check out your MongoLab database to ensure that 
--- queries.js
+++ queries.js
@@ -1,10 +1,23 @@
 /* Fill out these functions using Mongoose queries*/
+var mongoose = require('mongoose'), 
+    Listing = require('./ListingSchema.js'),
+    config = require('./config');
+
+mongoose.connect(config.db.uri);
+
 
 var findLibraryWest = function() {
   /* 
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
+   Listing.findOne({name:'Library West'}, function(err,list){
+	   if(err){
+		   throw err;
+	   }else{
+			console.log(list);
+	   }
+   });
 };
 var removeCable = function() {
   /*
@@ -12,20 +25,48 @@ var removeCable = function() {
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+   Listing.findOne({code:'CABL'}, function(err,list0){
+	   if(err)
+		   throw err;
+	   
+	   list0.remove(function(err){
+		   if(err){
+				throw err;
+		   }else{
+			   console.log(list0);
+			}
+	   });
+	   
+   });
 };
 var updatePhelpsLab = function() {
   /*
     Phelps Laboratory's address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
    */
+   Listing.findOne({name:'Phelps Laboratory'}, function(err, list1){
+	   if(err)
+		   throw err;
+	   list1.address = "1275 Center Drive Gainesville, FL 32611";
+	   list1.save(function(err){
+		   if(err)
+			   throw err;
+		   console.log(list1);
+	   });
+   });
 };
 var retrieveAllListings = function() {
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
+   Listing.find({}, function(err, list2){
+	   if(err)
+		   throw err;
+	   console.log(list2);
+   });
 };
 
 findLibraryWest();
-removeCable();
+removeCable(); 
 updatePhelpsLab();
 retrieveAllListings();
